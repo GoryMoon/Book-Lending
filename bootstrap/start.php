c36231b72fe407be74cfdca473d06b776a573194
Gustaf <gurreja@gmail.com>
2014-08-25 23:45:04 +0200

    Added third dev computer

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('gustaf-pc', 'LT-5TM0FT1'),
+	'local' => array('gustaf-pc', 'LT-5TM0FT1', 'Gustaf-HP'),
 
 ));
 
